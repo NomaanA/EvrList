cf811138e0c5dcdb52d59cace8e1c5ac965a01a6
Nomaan Ahgharian <nomaan@Nomaans-MacBook-Pro.local>
2016-09-17 23:46:26 -0400

    TODO list based on ionic

---
+++ app/pages/checklist/checklist.ts
@@ -0,0 +1,115 @@
+import {Component} from '@angular/core';
+import {NavController, AlertController, NavParams} from 'ionic-angular';
+import {Data} from "../../providers/data/data";
+import {HomePage} from '../home/home';
+import {ChecklistModel} from '../../providers/checklist-model/checklist-model';
+
+
+/*
+ Generated class for the ChecklistPage page.
+
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
+@Component({
+  templateUrl: 'build/pages/checklist/checklist.html',
+})
+export class ChecklistPage {
+
+  // title = HomePage.checklists[0];
+  // items = [];
+  public checklist: any;
+
+  constructor(private navCtrl: NavController, public dataService: Data, public alertCtrl: AlertController, params: NavParams) {
+    this.checklist = params.get("checklist");
+  }
+
+  addItem() {
+    // console.log(checklist);
+    let prompt = this.alertCtrl.create({
+      title: "Add",
+      message: "Enter the name",
+      inputs: [
+        {
+          name: 'name',
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            data.checked = false;
+            this.checklist.items.push(data);
+            // this.checklist.items.subscribe(update => {
+            //   this.save();
+            // });
+            this.save();
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  renameItem(item) {
+    let prompt = this.alertCtrl.create({
+      title: "Rename",
+      message: "Enter the new name",
+      inputs: [
+        {
+          name: 'name',
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            let index = this.checklist.items.indexOf(item);
+
+            if (index > -1) {
+              this.checklist.items[index].name = data.name;
+              this.save();
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  removeItem(item) {
+    let index = this.checklist.items.indexOf(item);
+
+    if(index > -1) {
+      this.checklist.items.splice(index, 1);
+    }
+  }
+
+  uncheckItems(item) {
+    //will set checked to false for all the items.
+    let index = this.checklist.items.indexOf(item);
+
+    console.log(index);
+  }
+
+  toggleItem(item) {
+    let index = this.checklist.items.indexOf(item);
+
+    if (index > -1) {
+      if(item.checked == false) {
+        this.checklist.items[index].checked = true;
+      }
+    }
+  }
+
+  save(): void {
+    console.log("saving");
+  }
+
+}
---
+++ app/providers/checklist-model/checklist-model.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from "rxjs/Rx";
+
+/*
+ Generated class for the ChecklistModel provider.
+
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+ */
+@Injectable()
+export class ChecklistModel {
+
+  checklist: any;
+  checklistObserver: any;
+
+  constructor(public title:String, public items: any[]) {
+    this.items = items;
+    this.checklist=  Observable.create(observer => {
+      this.checklistObserver = observer;
+    })
+  }
+
+  addItem(item):void {
+    this.items.push({
+      title: item,
+      checked: false
+    });
+
+    this.checklistObserver.next(true);
+  }
+  renameItem(item, title: void) {
+    let index = this.items.indexOf(item);
+
+    if (index > -1) {
+      this.items[index].title = title;
+    }
+
+    this.checklistObserver.next(true);
+  }
+
+  setTitle(title):void {
+    this.title = title;
+    this.checklistObserver.next(true);
+  }
+
+  toggleItem(item):void {
+    item.check= !item.checked;
+    this.checklistObserver.next(true);
+  }
+}
+
